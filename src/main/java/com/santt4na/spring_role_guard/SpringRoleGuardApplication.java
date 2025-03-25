cb3e83aace2c4304792d731fd5a97e098a9d026b
Santt4na <jhonnsantt4na@gmail.com>
2025-03-25 19:31:41 -0300

    Started Project

---
+++ src/main/java/com/santt4na/spring_role_guard/SpringRoleGuardApplication.java
@@ -0,0 +1,13 @@
+package com.santt4na.spring_role_guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoleGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRoleGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santt4na/spring_role_guard/SpringRoleGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santt4na.spring_role_guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRoleGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
