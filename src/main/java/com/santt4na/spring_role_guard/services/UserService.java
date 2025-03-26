4566e55d39b574638393e92edd1fbe5a12d9e0d8
Santt4na <jhonnsantt4na@gmail.com>
2025-03-25 23:28:15 -0300

    feat(userService): implement user creation, update, retrieval, and deletion with DTO mapping

--- src/main/java/com/santt4na/spring_role_guard/services/UserService.java
+++ src/main/java/com/santt4na/spring_role_guard/services/UserService.java
@@ -1,5 +1,66 @@
 package com.santt4na.spring_role_guard.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.santt4na.spring_role_guard.dto.UserDto;
+import com.santt4na.spring_role_guard.exception.UserNotFoundException;
+import com.santt4na.spring_role_guard.mappers.UserMapper;
+import com.santt4na.spring_role_guard.model.User;
+import com.santt4na.spring_role_guard.repository.UserRepository;
+
+@Service
 public class UserService {
 
+  private UserRepository repository;
+  private UserMapper mapper;
+
+  public UserDto createUser(UserDto userDto) {
+    User user = new User();
+    user.setName(userDto.getName());
+    user.setEmail(userDto.getEmail());
+    user.setPassword(userDto.getPassword());
+    user.setUsername(userDto.getUsername());
+    user.setIsBlocked(userDto.getIsBlocked());
+
+    User savedUser = repository.save(user);
+    return mapper.toDto(savedUser);
+  }
+
+  public UserDto updateUser(Long id, UserDto userDto) {
+    User user = repository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
+
+    user.setName(userDto.getName());
+    user.setEmail(userDto.getEmail());
+    user.setUsername(userDto.getUsername());
+    user.setIsBlocked(userDto.getIsBlocked());
+
+    User updatedUser = repository.save(user);
+    return mapper.toDto(updatedUser);
+  }
+
+  public UserService(UserRepository repository, UserMapper mapper) {
+    this.repository = repository;
+    this.mapper = mapper;
+  }
+
+  public List<UserDto> findAllUsers() {
+    List<User> users = repository.findAll();
+    return users.stream().map(mapper::toDto).collect(Collectors.toList());
+  }
+
+  public Optional<UserDto> findUserDtoById(Long id) {
+    return repository.findById(id)
+        .map(mapper::toDto);
+  }
+
+  public void deleteUser(Long id) {
+    User user = repository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
+    repository.delete(user);
+  }
 }
